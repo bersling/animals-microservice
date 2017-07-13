83afa1afdfa8fa04ff6660fba0eb8efe5137a480
bersling <bersling@gmail.com>
2017-07-13 12:36:11 +0200

    fix;

--- server.js
+++ server.js
@@ -2,22 +2,18 @@ const find = require("find");
 const express = require('express');
 const path = require('path');
 const server = express();
+const cors = require('cors');
 require('fs-lock')({'file_accessdir': [ __dirname ], 'open_basedir': [ __dirname ]});
 const fs = require('fs');
 
-server.use(function(req, res, next) { // Allow CORS
-  res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+server.use(cors());
 
 server.get('/find', function (req, res) {
   if (req.query.q) {
-    const pattern = new RegExp(`(.*?)${req.query.q}(.*?).svg`)
+    const pattern = new RegExp(`(.*?)${req.query.q}(.*?).svg`);
     find.file(pattern, path.join(__dirname, '/animals'), function(files) {
       const randomFile = files[Math.floor(Math.random()*files.length)];
-      const relPath = path.relative(__dirname, randomFile);
+      const relPath = randomFile ? path.relative(__dirname, randomFile) : 'animals/not-found.svg';
       res.send({
         path: relPath,
         urlEncodedPath: encodeURIComponent(relPath)
